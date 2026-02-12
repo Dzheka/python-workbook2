7d1e5ad920c0919fbbd85b67d94826793b2e5f91
Temur <temur_007@Temurs-MacBook-Air.local>
2026-02-12 13:19:47 +0500

    recap assignment

---
+++ class_basics/05_library/library.py
@@ -0,0 +1,39 @@
+class Book():
+    def __init__(self,title,author, isbn) -> None:
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+    def __repr__(self) -> str:
+        return f"{self.title} by {self.author}"
+
+
+
+class Library(name):
+    global books
+    books = []
+    def add_book(self, book):
+        books.append[book]
+        return f"Added: {book}"
+
+    def find_book(self,title):
+        for book in books:
+            if book == title:
+                return self.books
+        return None
+
+    def list_books(self):
+        for book in self.books:
+            print(f"{book}\n")
+
+library = Library("City Library")
+book1 = Book("1984", "George Orwell", "123")
+book2 = Book("Dune", "Frank Herbert", "456")
+
+library.add_book(book1)
+library.add_book(book2)
+
+found = library.find_book("1984")
+print(found)  # 1984 by George Orwell
+
+library.list_books()
