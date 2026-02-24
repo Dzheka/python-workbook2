a96a412869495fa934ad9bf2f5c0f194f2476a55
SENZHO <syufu3@Gmail.com>
2026-02-24 13:12:55 +0500

    commit

---
+++ dunder_methods_encapsulation/fraction/fraction.py
@@ -0,0 +1,31 @@
+from math import gcd
+
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+    
+    def __eq__(self, other):
+        return self.numerator * other.denominator == self.denominator * other.numerator
+    
+    def __add__(self, other):
+        new_num = self.numerator * other.denominator + other.numerator * self.denominator
+        new_den = self.denominator * other.denominator
+        return Fraction(new_num, new_den)
+    
+    def __lt__(self, other):
+        return self.numerator * other.denominator < self.denominator * other.numerator
+    
+a = Fraction(1, 2)
+b = Fraction(1, 3)
+c = Fraction(2, 4)
+
+print(a)
+print(a == c)
+print(a < b)
+
+d = a + b
+print(d)
