9820fb287edc73101109439ec9bfa4d858c905d4
otabek <otabekbarotov15@gmail.com>
2026-02-26 12:58:37 +0500

    dunder methods

--- dunder_methods_encapsulation/fraction/fraction.py
+++ dunder_methods_encapsulation/fraction/fraction.py
@@ -1,19 +1,35 @@
-from math import  gcd
-
+import math
 class Fraction:
-    def __init__(self, numerator, denominator):
+    def __init__(self,numerator,denominator):
         self.numerator = numerator
         self.denominator = denominator
-        common = gcd(numerator, denominator)
+        gcd = math.gcd(numerator, denominator)
 
-        self.numerator = numerator // common
-        self.denominator = denominator // common
 
     def __str__(self):
         return f"{self.numerator}/{self.denominator}"
 
     def __eq__(self, other):
-        if isinstance(other, Fraction):
-            return (self.numerator == other.numerator and
-                    self.denominator == other.denominator)
+        return self.numerator * other.denominator == other.numerator * self.denominator
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator,new_denominator)
+
+    def __lt__(self, other):
+        if self.numerator * other.denominator < other.numerator * self.denominator:
+            return True
         return False
+
+
+a = Fraction(1, 2)
+b = Fraction(1, 3)
+c = Fraction(2, 4)
+
+print(a)          # 1/2
+print(a == c)     # True  (1/2 == 2/4)
+print(a < b)      # False (1/2 > 1/3)
+
+d = a + b
+print(d)          # 5/6
