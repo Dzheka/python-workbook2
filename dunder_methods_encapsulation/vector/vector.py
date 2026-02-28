3cff0ca1c9d9c7ca453ed6ae96e483146bbb374a
Saduloeva <osiyasadulloeva8@gmail.com>
2026-02-28 21:12:23 +0500

    dunder_methods_encapsulation

--- dunder_methods_encapsulation/vector/vector.py
+++ dunder_methods_encapsulation/vector/vector.py
@@ -1,28 +1,28 @@
 class RGBColor:
-    def __init__(self, r, g, b):
-        self.r = r
-        self.g = g
-        self.b = b
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
 
     def __str__(self):
-        return f"rgb({self.r}, {self.g}, {self.b})"
+        return f"rgb({self.red}, {self.green}, {self.blue})"
 
     def __repr__(self):
-        return f"RGBColor({self.r}, {self.g}, {self.b})"
+        return f"RGBColor({self.red}, {self.green}, {self.blue})"
 
     def __eq__(self, other):
-        return self.r == other.r and self.g == other.g and self.b == other.b
+        return self.red == other.red and self.green == other.green and self.blue == other.blue
 
     def __add__(self, other):
-        return RGBColor((self.r + other.r) // 2, (self.g + other.g) // 2, (self.b + other.b) // 2)
+        return RGBColor((self.red + other.red) // 2, (self.green + other.green) // 2, (self.blue + other.blue) // 2)
 
     def __contains__(self, channel):
         if channel == "red":
-            return self.r > 0
+            return self.red > 0
         elif channel == "green":
-            return self.g > 0
+            return self.green > 0
         elif channel == "blue":
-            return self.b > 0
+            return self.blue > 0
         return False
 
 
