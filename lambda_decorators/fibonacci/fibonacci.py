1c59e134a46579aba9098a37dd23cbdc63e8f421
otabek <otabekbarotov15@gmail.com>
2026-03-03 18:29:22 +0500

    solution to lambda decorators added

---
+++ lambda_decorators/fibonacci/fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n: int) -> int:
+    pass
+
+if __name__ == "__main__":
+    print(fibonacci(0))   # 0
+    print(fibonacci(1))   # 1
+    print(fibonacci(6))   # 8
+    print(fibonacci(10))  # 55
---
+++ oop_principles/1_encapsulation/12_counter/counter.py
@@ -0,0 +1,3 @@
+class Counter:
+    def __init__(self,min_value = 0, max_value = 100):
+        pass
